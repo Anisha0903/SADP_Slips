5ea21baca6f0f2fee33625e18e76388798a489f0
Anisha0903 <anishabpeth@gmail.com>
2025-11-17 13:27:52 +0530

    Create Slip20.java

---
+++ Slip20.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+class LowerCaseInputStream extends FilterInputStream {
+    protected LowerCaseInputStream(InputStream in) {
+        super(in);
+    }
+
+    @Override
+    public int read() throws IOException {
+        int c = super.read();
+        return (c == -1 ? c : Character.toLowerCase((char) c));
+    }
+
+    @Override
+    public int read(byte[] b, int offset, int len) throws IOException {
+        int result = super.read(b, offset, len);
+        if (result != -1) {
+            for (int i = offset; i < offset + result; i++) {
+                b[i] = (byte) Character.toLowerCase((char) b[i]);
+            }
+        }
+        return result;
+    }
+}
+
+public class Slip20 {
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("Enter your text: ");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String userInput = br.readLine();
+
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(userInput.getBytes());
+
+        LowerCaseInputStream lowerIn = new LowerCaseInputStream(byteIn);
+
+        System.out.print("Converted to lowercase: ");
+
+        int c;
+        while ((c = lowerIn.read()) != -1) {
+            System.out.print((char) c);
+        }
+
+        lowerIn.close();
+    }
+}
